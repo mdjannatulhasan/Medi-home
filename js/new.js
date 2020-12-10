e23defef738b881382ddb1d485c6b4f033f010fc
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2020-12-10 14:14:30 +0600

    1

--- js/new.js
+++ js/new.js
@@ -1,12 +1,35 @@
-$(document).ready(function(){
-    $('.your-class').slick({
-        prevArrow:'.arrow_prev',
-        nextArrow:'.arrow_next',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrow:true,
-        dots:true,
-    });
-  });
+// $(document).ready(function(){
+//     $('.your-class').slick({
+//         prevArrow:'.arrow_prev',
+//         nextArrow:'.arrow_next',
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         arrow:true,
+//         dots:true,
+//     });
+//   });
+$('.main-content .owl-carousel').owlCarousel({
+    items:1,
+    // stagePadding: 50,
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+        'PRVE',
+        'NEXT',
+    ],
+    navContainer: '.main-content .custom-nav',
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 1
+        },
+        1000:{
+            items: 1
+        }
+    }
+});
