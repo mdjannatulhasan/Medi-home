9aa34b4a0b13f63165198871d06671534080a898
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2020-12-13 17:00:52 +0600

    1

--- js/new.js
+++ js/new.js
@@ -39,18 +39,31 @@ document.onmousedown = mousehandler;
 document.onmouseup = mousehandler;
 
 /********** Masonry With Filter************/
-var $grid = $('.grid');
-$grid.isotope({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    masonry: {
-        columnWidth: '.grid-sizer'
+
+
+
+$('.team .owl-carousel').owlCarousel({
+    items:4,
+    // autoplay:true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    loop: true,
+    nav: true,
+    navText: [
+        '<div style="font-size:20px; padding: 8px 20px;letter-spacing: 2px;"><div>PR</div><div style="margin-top:-10px;">EV</div></div>',
+        '<div style="font-size:20px; padding: 8px 20px;letter-spacing: 2px;"><div>NE</div><div style="margin-top:-10px;">XT</div></div>',
+    ],
+    navContainer: '.team .custom-nav',
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 2
+        },
+        1000:{
+            items: 4
+        }
     }
 });
-$('.filter-button-group').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-});
-
-
 
