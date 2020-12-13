8c103be03765a604b610f6c2a9a5018442c8cae8
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2020-12-13 15:10:08 +0600

    1

--- js/new.js
+++ js/new.js
@@ -53,17 +53,4 @@ $('.filter-button-group').on( 'click', 'button', function() {
 });
 
 
-/********** active setter *********/
 
-var btnContainer = document.getElementById("activeSet");
-
-
-var btns = btnContainer.getElementsByClassName("navBtn");
-
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
